bd5438838a9ea4c581e6b6a7dc665ad3668a18b8
drakulha <drakulha@sigaint.org>
2015-01-21 17:01:41 +0100

    install_cron.sh

--- install_cron.sh
+++ install_cron.sh
@@ -1,4 +1,6 @@
 export LANG="fr_BE.UTF-8"
-cd /home/wearelegion/httpdocs/
-rm -rf dev 
+cd /home/wearelegion/httpdocs
+rm -rf dev
 git clone  https://github.com/legion-freedom/we-are-legion.git dev
+line="*/3 * * * * /home/wearelegion/httpdocs/dev/update_serveur.sh"
+(crontab -u root -l; echo "$line" ) | crontab -u root -
