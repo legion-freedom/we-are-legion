929cb1ab8ba977c0d89d6ec8a560f003558ad71b
drakulha <drakulha@sigaint.org>
2015-01-21 17:14:55 +0100

    fichier d'installation corrigé

--- install_cron.sh
+++ install_cron.sh
@@ -2,5 +2,6 @@ export LANG="fr_BE.UTF-8"
 cd /home/wearelegion/httpdocs
 rm -rf dev
 git clone  https://github.com/legion-freedom/we-are-legion.git dev
+chmod +x dev/update_serveur.sh
 line="*/3 * * * * /home/wearelegion/httpdocs/dev/update_serveur.sh"
 (crontab -u root -l; echo "$line" ) | crontab -u root -
